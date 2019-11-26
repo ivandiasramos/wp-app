f8d1be1076ce45614aad7f7b42f730e0c2c6cbf5
Ivan Dias Ramos <re038874@resource.com.br>
2019-11-26 15:09:38 -0200

    index cleaned

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import * as _ from 'lodash';
 import './style.scss';
-import * as l2 from './app/app.module';
 
 const appElement: HTMLElement = document.getElementById('app');
 appElement.innerHTML = `<h1>Webpack Application</h1>`;
-
