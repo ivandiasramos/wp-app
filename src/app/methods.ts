98fdc8ae9261fe1272c5ad1aa9e92cf34da92c12
Ivan Dias Ramos <re038874@resource.com.br>
2019-11-26 15:10:25 -0200

    new methods added

--- src/app/methods.ts
+++ src/app/methods.ts
@@ -26,3 +26,59 @@ export function chunk(arr, size) {
 
   return newArray;
 }
+
+export function compact(arr) {
+  return arr.filter(Boolean);
+}
+
+export function concat(arr, ...values) {
+  return [...arr, ...values];
+}
+
+export function difference(arr, values) {
+  const newArray = arr;
+
+  remove(newArray, item => values.includes(item));
+
+  return newArray;
+}
+
+export function isArray(value) {
+  return value && typeof value === 'object' && value.constructor === Array;
+}
+
+export function isObject(value) {
+  return value && typeof value === 'object' && value.constructor === Object;
+}
+
+export function isFunction (value) {
+  return typeof value === 'function';
+}
+
+export function isNull(value) {
+  return value === null;
+}
+  
+export function isUndefined(value) {
+  return typeof value === 'undefined';
+}
+
+export function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
+export function isRegExp (value) {
+  return value && typeof value === 'object' && value.constructor === RegExp;
+}
+
+export function isError (value) {
+  return value instanceof Error && typeof value.message !== 'undefined';
+}
+
+export function isDate (value) {
+  return value instanceof Date;
+}
+
+export function isSymbol (value) {
+  return typeof value === 'symbol';
+}
