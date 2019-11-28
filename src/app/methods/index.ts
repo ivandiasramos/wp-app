fea57c25e5cbc2a18056e1b936406fc0218cddc2
Ivan Dias Ramos <re038874@resource.com.br>
2019-11-28 18:29:18 -0200

    methods module updated

--- src/app/methods/index.ts
+++ src/app/methods/index.ts
@@ -1,3 +1,6 @@
 export { remove } from './remove/remove';
 export { isArray } from './is-array/is-array';
 export { chunk } from './chunk/chunk';
+export { isObject } from './is-object/is-object';
+export { isString } from './is-string/is-string';
+export { deepFilter } from './deep-filter/deep-filter';
